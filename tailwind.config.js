b4f60d9021500476ce61f8fe46f3cfe5db0ff08c
amirmahdirahimzadeh2009 <158206164+amirmahdirahimzadeh2009@users.noreply.github.com>
2024-05-22 00:50:28 +0330

    responsive size 1440

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        black: "#151515",
+        background: "#E2E2E2",
+        gray: "#222831",
+        gray2: "#DDDDDD",
+      },
+      fontFamily: {
+        Abhaya: "Abhaya",
+      },
+    },
+  },
+  plugins: [],
+};
